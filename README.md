bc65fb179102fdbf07e2661fd75e38ca6f60d143
Calvin Buckley <calvin@cmpct.info>
2020-02-14 14:38:46 -0400

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+This program will run xz with the specified files (`-f` flags) and threads
+(`-t` flags; 0 for all cores xz can find). There can be multiple of each flags
+specified. The values are tab-seperated for easy formatting.
+
+Should run on Perl 5.10 (maybe older if the modules are there) with
+`Time::HiRes` and `Getopt::Long`.
+
+# TODO
+
+* Automatically detect proc/core/thread hierarchy if no `-t` given
+
+# Links
+
+* [Corpus we use](http://mattmahoney.net/dc/textdata.html)
+* [tsundoku's results](https://wiki.tsundoku.ne.jp/XZ_%E5%9C%A7%E7%B8%AE%E8%A9%A6%E9%A8%93)
+* [John's results](https://ljck.org/doku.php?id=xzbench)
---
+++ xzbench.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+
+# xz testbench - run a few iterations of xz with different thread counts
+
+use Getopt::Long;
+use Time::HiRes;
+
+sub delete_file {
+	my $delete = shift;
+	my $delete_ext = "$delete.xz";
+	unlink $delete_ext;
+}
+
+sub run {
+	my $cores = shift;
+	my $file = shift;
+	my $cmd = "xz --keep -T$cores $file";
+#	print "Running with $cores threads on $file\n";
+	my $pid = fork();
+	if ($pid == 0) { # child forks again to run xz
+#		print "$cmd\n";
+		my $r = [Time::HiRes::gettimeofday()];
+		system($cmd);
+		($u, $s, $cu, $cs) = times;
+		my $rdiff = Time::HiRes::tv_interval($r);
+		print "$file\t$cores\t$rdiff\t$cs\t$cu\n";
+		exit;
+	} else { # parent needs to block on child
+		waitpid($pid, 0);
+	}
+}
+
+my @core_count;
+my @files;
+
+GetOptions('thread|t=i' => \@core_count, 'file|f=s' => \@files);
+
+print "file\tthreads\treal\tsys\tuser\n";
+
+foreach my $file (@files) {
+	foreach my $cores (@core_count) {
+		delete_file($file);
+		run($cores, $file);
+		delete_file($file);
+	}
+}
